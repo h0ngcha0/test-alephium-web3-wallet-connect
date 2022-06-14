070156e28ede9fcd1f14453bf716d13adb1e20d2
Hongchao Liu <lbs.lhc@gmail.com>
2022-06-14 10:59:30 +0200

    Pin versions

--- src/App.tsx
+++ src/App.tsx
@@ -5,38 +5,55 @@ import { explorer, Contract, Script } from '@alephium/web3'
 import contractJson from './artifacts/greeter.ral.json'
 import scriptJson from './artifacts/greeter_main.ral.json'
 
-function Dashboard() {
-  const api = new explorer.Api<null>({ baseUrl: 'https://mainnet-backend.alephium.org' })
-  const contract = Contract.fromJson(contractJson).toString()
-  const script = Script.fromJson(scriptJson).toString()
-  const [blocks, setBlocks] = useState('')
+import WalletConnectClient from '@walletconnect/client'
 
-  useEffect(() => {
-    async function fetchBlocks() {
-      const blocks = (await api.blocks.getBlocks({ page: 1 })).total
-      setBlocks(JSON.stringify(blocks))
+function Dashboard() {
+    const api = new explorer.Api<null>({ baseUrl: 'https://mainnet-backend.alephium.org' })
+    const contract = Contract.fromJson(contractJson).toString()
+    const script = Script.fromJson(scriptJson).toString()
+    const [blocks, setBlocks] = useState('')
+
+    async function walletConnectCallback() {
+        const walletConnect = await WalletConnectClient.init({
+            projectId: '6e2562e43678dd68a9070a62b6d52207',
+            relayUrl: 'wss://relay.walletconnect.com',
+            metadata: {
+                name: 'Alphium NFT',
+                description: 'Alpephium NFT Marketplace',
+                url: 'https://walletconnect.com/',
+                icons: ['https://walletconnect.com/walletconnect-logo.png']
+            }
+        })
+
+        console.log(walletConnect)
     }
 
-    fetchBlocks()
-  })
+    useEffect(() => {
+        async function fetchBlocks() {
+            const blocks = (await api.blocks.getBlocks({ page: 1 })).total
+            setBlocks(JSON.stringify(blocks))
+        }
 
-  return (
-    <div>
-      <p>blocks: {blocks}</p>
-      <p>contract: {contract}</p>
-      <p>script: {script}</p>
-    </div>
-  )
+        fetchBlocks()
+    })
+
+    return (
+        <div>
+            <p>blocks: {blocks}</p>
+            <p>contract: {contract}</p>
+            <p>script: {script}</p>
+        </div>
+    )
 }
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <Dashboard />
-      </header>
-    </div>
-  )
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <Dashboard />
+            </header>
+        </div>
+    )
 }
 
 export default App
